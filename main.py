f8c9164b8a5dc6ce9681a8cc704656cc1429ee3a
jared <188791753+brownj82@users.noreply.github.com>
2025-02-21 12:09:36 -0500

    Fix

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ row_selection_start = 7
 row_num_end = 440
 num_selections = 5
 
-selections = random.sample(range(row_selection_start + 1, row_num_end), num_selections)
+selections = random.sample(range(row_selection_start, row_num_end), num_selections)
 print('Selections:', selections)
 
